932e9669e9ac7e210a597c8263d2d175643f2958
Ruslan Belyy <ruslanbelyy@gmail.com>
2023-07-02 20:28:43 -0400

    changed code to check for instock rather then soldout

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const twilio = require("twilio")(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
 const itemsToCheck = [
   "https://store.ui.com/collections/unifi-protect/products/g4-doorbell-pro",
-  "https://store.ui.com/us/en/products/uacc-adapter-dbpoe"
+  "https://store.ui.com/us/en/products/uacc-adapter-dbpoe",
+  "https://store.ui.com/us/en/pro/category/all-cameras-nvrs/products/uacc-g5-enhancer"
 ];
 
 function checkItems() {
@@ -26,8 +27,8 @@ function checkItems() {
     result.forEach((res) => {
       const root = parse(res.data);
       const title = root.querySelector('title').text.split('-')[0].trim();
-      const soldOut = root.querySelector('button[label="Sold Out"]');
-      if (!soldOut) sendText(`${title} is in stock`);
+      const addToCart = root.querySelector('button[label="Add to Cart"]');
+      if (addToCart) sendText(`${title} is in stock`);
     });
   });
 }
@@ -41,14 +42,12 @@ function sendText(message) {
 }
 
 //Run every hour and check website for items.
-cron.schedule("*/10 * * * *", () => {
-  console.log("Checking Items");
+cron.schedule("* * * * *", () => {
   checkItems();
 });
 
 //Run once a week at 12:00 on Monday to ensure texting is working
 cron.schedule("0 12 * * 1", () => {
-  console.log("Weekly Text that this works");
   sendText("Unify Works");
 });
 
